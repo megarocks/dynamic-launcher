41d38a8f67b222968a354c873242c2a764f2568a
Vitaly <vitaly.kiktiov@gmail.com>
2018-11-16 13:28:38 +0200

    add logger and autoupdater

--- public/electron.js
+++ public/electron.js
@@ -1,12 +1,17 @@
 const {app, BrowserWindow} = require('electron');
+const { autoUpdater } = require('electron-updater')
+const log = require('electron-log');
 
-const path = require('path');
-const url = require('url');
-const isDev = require('electron-is-dev');
+const path = require('path')
+const url = require('url')
+const isDev = require('electron-is-dev')
 
 let os = require("os")
 
-console.log(os.userInfo().username)
+
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
+log.info('App starting...');
 
 let mainWindow;
 
@@ -23,9 +28,42 @@ function createWindow() {
   mainWindow.on('closed', () => mainWindow = null);
 }
 
+function sendStatusToWindow(text) {
+  log.info(text);
+  mainWindow.webContents.send('message', text);
+}
+
+
+autoUpdater.on('checking-for-update', () => {
+  sendStatusToWindow('Checking for update...');
+})
+autoUpdater.on('update-available', (info) => {
+  sendStatusToWindow('Update available.');
+})
+autoUpdater.on('update-not-available', (info) => {
+  sendStatusToWindow('Update not available.');
+})
+autoUpdater.on('error', (err) => {
+  sendStatusToWindow('Error in auto-updater. ' + err);
+})
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  sendStatusToWindow(log_message);
+})
+autoUpdater.on('update-downloaded', (info) => {
+  sendStatusToWindow('Update downloaded');
+  autoUpdater.quitAndInstall();
+});
+
+
 app.commandLine.appendSwitch('disable-http-cache')
 
-app.on('ready', createWindow);
+app.on('ready', async () => {
+  createWindow()
+  await autoUpdater.checkForUpdates();
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
